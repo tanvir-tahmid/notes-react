6b4fcf86a8088fd2e56f751dff4e11afe4fd60ec
Tanvir Tahmid <tanv.mhn@gmail.com>
2021-12-29 20:27:09 +0600

    commit

---
+++ src/assets/data.js
@@ -0,0 +1,23 @@
+let notes = [
+    {
+        "id": 1,
+      "body": "eat",
+      "updated": "2021-08-31T16:18:41.809Z",
+        "created": "2021-07-13T21:54:16.235392Z"
+    },
+    {
+        "id": 2,
+        "body": "sleep",
+        "updated": "2021-08-30T21:34:55.203Z",
+        "created": "2021-07-13T00:17:13.289897Z"
+    },
+    {
+        "id": 3,
+      "body": "repeat",
+      "updated": "2021-07-13T19:46:12.187306Z",
+        "created": "2021-07-13T00:16:22.399841Z"
+    }
+]
+
+
+export default notes;
